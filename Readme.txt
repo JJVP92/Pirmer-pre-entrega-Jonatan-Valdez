b7080cd6d336536f010b8b16d542fbdfeba492e0
Jonatan Valdez <jonatanvaldez92@hotmail.com>
2023-04-17 20:51:32 -0300

    Se modifica para la 2da pre entrega

--- Readme.txt
+++ Readme.txt
@@ -1,9 +1,16 @@
-1ra Pre-entrega del proyecto final de javasript
+2da Pre-entrega del proyecto final de javasript
 
-El objetivo de esta pre entrega son:
-Crear un algoritmo con un condicional
-Crear un algoritmo usando un ciclo
-armar un simulador interactivo, la estructura final del proyeto
+La segunda entrega consiste en:
+
+- Todo lo que incluía la primera entrega, agregando:
+
+- Funciones - Declarar e invocar funciones
+
+- Objetos - Crear objetos a partir de una clase
+
+- Arrays - Cargar elementos en un array con metodos vistos en clase
+
+- Funciones de Orden Superior - Utilizar al menos una de los métodos vistos en esta clase de funciones de orden superior I y/o II.
 ................................................................
 
 Este trabajo fue basado en el proyecto final del curso de desarrollo web. 
@@ -24,4 +31,14 @@ El algoritmo tiene el siguente desarrollo :
 6. Si nos da el si, nos pide que ingresemos dicho cupon, que lo vuelve a comparar si es el correcto
    que en este caso es "JJVP".
 7. De lo contrario nos deja ingresar a la pagina sin descuentos
+
+*****************AGREGADOS EN 2da PRE ENTREGA*******************************************
+
+8. Se cargo un arrays de objetos que contiene los nombres de los pruductos ofrecidos y sus precios
+9. Se cargo un arrays de lugares de trabajo, donde se atravez de un push agrega otro elemento
+10. Atravez de la herramienta promt tenemos un objeto de clases conla funcion constructora
+11. Se agrego la posibilidad de modificar los precios de los elemntos atravez de una validacion de contraseña  
+    donde si es correcta, nos da la posibilidad de modifciar el array de objetos con el valor que ingresamos atravez
+    de un promt, donde lo divide por 100 y asi calcular el aumento que le queremos dar y transformalo con la funcion de orden
+    superiro .map
 
--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 const vacio= "";
+const contraseña= 1992
 let nombreempresa = prompt("Cual es el nombre de tu empresa?")
 let nombre= prompt("Cual es tu nombre?")
 while (nombreempresa == vacio || nombre ==vacio) {
@@ -20,3 +21,52 @@ else{
     alert("Ingresas a la pagina sin descuentos")
 }
 
+const productos =[
+    {nombreproducto: "CarteInterior", precioxm2: 4500},
+    {nombreproducto: "CartelExterior", precioxm2: 5605},
+    {nombreproducto: "Redlinepuma", precioxm2:50034},
+    {nombreproducto: "PunteraIsla", precioxm2:3043},
+]
+const lugaresdeservicios= ["GBA", "CABA","Mendoza"]
+lugaresdeservicios.push("Cordoba");
+console.log(lugaresdeservicios)
+
+let apellido = prompt ("Cual es tu apellido?");
+let edad = prompt ("Cual es tu edad?");
+let cuit = prompt ("Cual es tu CUIT?");
+let residencia = prompt("Donde queres hacer el trabajo?")
+
+class Datos{
+    constructor(nombre, apellido, edad, cuit, Residencia){
+    this.nombre = nombre;
+    this.apellido = apellido;
+    this.edad = edad;
+    this.cuit = cuit;
+    this.Residencia = Residencia;}
+}
+const usuario1 = new Datos (nombre, apellido, edad, cuit, residencia);
+console.log (usuario1);
+
+let respuesta = prompt("Actualizar los precios?");
+let inflacion=prompt("ingresa el porcentaje de la inflacion")
+let porcentaje= inflacion/100
+
+if (respuesta.toLowerCase() == "si") {
+  let contraseña = prompt("Ingresa contraseña:");
+  if (contraseña == contraseña) {
+    console.log("Las contraseñas coinciden");
+   
+   const preciosActualizados = productos.map((producto) => {
+     return {
+         nombre: producto.nombre,
+         precio: producto.precioxm2 * porcentaje
+     }
+ });
+ console.log(productos);
+ console.log(preciosActualizados);
+  } else {
+    console.log("Las contraseñas no coinciden");
+  }
+} else {
+ console.log("Acceso denegado.");
+}
